af776b088405ae406e95cc66f08701d99fdd4405
LucaLabio <lucalabio1@gmail.com>
2020-03-16 12:55:14 -0300

    correção do exercicio 03 dia 5

--- dia 5/exercicio03.py
+++ dia 5/exercicio03.py
@@ -10,3 +10,20 @@ ordem posicional e somar esses resultados. O último digito do número
 obtido é o dígito verificador.
 – Ex: 7 x 1 + 6 x 2 + 7 x 3 = 40 (dígito verificador ! 0).
 • PS: Use o operador % e a divisão inteira para obter o dígito verificador. """
+nconta = int(input("Digite o numero da conta: "))
+n1 = int(nconta / 100)
+n2 = int((nconta - (n1*100))/10)
+n3 = int(nconta - (n1*100 + n2*10))
+inverso = int((n3*100) + (n2*10) + n1)
+soma = nconta + inverso
+multiplicacao = n1 * 1 + n2 * 2 + n3 * 3
+verificador = int(multiplicacao / 10)
+verificador = multiplicacao - verificador*10
+print("O seu Digito verificador e: " + str(verificador))
+print(n1)
+print(n2)
+print(n3)
+print(inverso)
+print(soma)
+print(multiplicacao)
+print(verificador)
